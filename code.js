958c81224fb68284bce71b694af08661c529a61d
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-10 22:46:21 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function insertionSortReverse(array) {
   if (array.length == 0 || array.length == 1) {
     return array;
   }
-  for(var i = array.length - 1; i >=0 <; i--) {
+  for(var i = array.length - 1; i >= 0; i--) {
     var val = array[i];
     for(j = i+1; j < array.length && array[j] < val; j++) {
       array[j-1] = array[j];
